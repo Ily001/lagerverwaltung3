4687857ff1b43cfa57ffc086504224da8ec63da4
Ily <ilham.belahcen@hs-rm.de>
2023-02-21 17:57:11 +0100

    Hurra! Barcode ist da :D

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import './App.css';
 import Header from './components/Header';
 import NavBar from './components/Navbar/NavBar';
-import Barcode from './components/Barcode';
+import Barcode from './components/BarcodeGenerator';
+import BarcodeScanner from './components/BarcodeScanner';
+
 
 function App() {
   return (
@@ -9,8 +11,11 @@ function App() {
       <Header/>
       <div className='w-full min-h-[90vh]'>
         <NavBar/>
-        <div className='grid grid-cols-1 xl:grid-cols-5 col-span-10 w-full'>
+        <div className='grid grid-cols-1 xl:grid-cols-1 col-span-10 w-full'>
+        <BarcodeScanner />
         <Barcode/>
+
+  
         </div>
       </div>
     </div>
